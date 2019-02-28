04e8e3b124ee9c3271292e39979606bbb00f777d
juamecos <juamecos@gmail.com>
2019-02-28 08:32:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+  // let parent = document.querySelector('.parallax');
+  let children = document.getElementsByClassName('parallax__layer');
+  let eagle = document.querySelector(".parallax__layer--eagle");
+  console.log(window.pageYOffset)
+  for(let i = 0; i < children.length; i++) {
+    children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+  }
+  eagle.style.transform = `translate(-${window.pageYOffset*2.5}px, ${window.pageYOffset/2}px)`;
+}, false)
